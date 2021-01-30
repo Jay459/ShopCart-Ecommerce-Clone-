fa36e41372d30a2e66c69657ddc8c020b0155013
jay459 <anigounigoud@gmail.com>
2021-01-30 15:11:21 +0530

    adding new product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,12 @@
+const Product = require('./../models/productModel');
+
+exports.newProduct = async (req,res,next) => {
+    const product = await Product.create(req.body);
+    res.status(200).json({ 
+        success: true,
+        product: product
+    })
+}
 
 exports.getAllProuducts = (req,res,next) =>{
     res.status(200).json({
