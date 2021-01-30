af416eddf18ec2f9dd946aaedd4ea6ca4cd5540d
jay459 <anigounigoud@gmail.com>
2021-01-30 15:11:48 +0530

    modified routes

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,4 +5,6 @@ const productcontroller = require('./../controllers/productController');
 
 router.get('/getallproducts', productcontroller.getAllProuducts);
 
+router.post('/newproduct', productcontroller.newProduct);
+
 module.exports = router;
