7b04a1136cf2ff92afa9f0a00193cf2f72f0f6cd
jay459 <anigounigoud@gmail.com>
2021-01-31 22:35:06 +0530

    modified index file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const errormiddleware = require('../backend/middleware/errormiddleware');
 const productroute = require('./routes/product');
+
 
 //config
 require('dotenv').config({path:__dirname + '/.env'}) 
@@ -14,6 +16,9 @@ app.use(express.json());
 //routes
 app.use('/product', productroute);
 
+
+//middleware
+app.use(errormiddleware);
 
 app.listen(port, () => {
     console.log(`Server started on port : ${port}`);
