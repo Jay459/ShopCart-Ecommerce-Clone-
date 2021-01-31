b1855de2d976f33562f4e94c30ef51c9707d4fa3
jay459 <anigounigoud@gmail.com>
2021-01-31 22:37:21 +0530

    modified controllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,8 @@
 const Product = require('./../models/productModel');
 
+
+//creating new product
+
 exports.newProduct = async (req,res,next) => {
     const product = await Product.create(req.body);
     res.status(200).json({ 
@@ -8,9 +11,75 @@ exports.newProduct = async (req,res,next) => {
     })
 }
 
-exports.getAllProuducts = (req,res,next) =>{
+
+// displaying all the products
+exports.getAllProuducts = async (req,res,next) =>{
+    const products = await Product.find();
+    if(products.length <=0){
+        return res.json({
+            message:"There are no products"
+        })
+    }
+
     res.status(200).json({
         success:"true",
-        message:"This will show the products in the database."
+        products: products
+    })
+}
+
+
+//get a single product by ID
+
+exports.getSingleProduct = async (req, res, next) =>{
+
+    const product = await Product.findById(req.params.id);
+    
+    if(!product)
+    {
+        return next(new Error("Product not found",404));
+    }
+
+
+    res.status(200).json({
+        success:"true",
+        product: product
+    })
+}
+
+//updating the products
+
+exports.updateProductById = async (req, res, next) =>{
+    let product = await Product.findById(req.params.id);
+
+    if(!product){
+        return next(new Error("Product not found", 404));
+    }
+
+
+    product = await Product.findByIdAndUpdate(req.params.id , req.body , {
+        new : true,
+        runValidators:true,
+        useFindAndModify:false
+    })
+
+    res.status(200).json({
+        success:"true",
+        updatedProduct:product
+    })
+}
+
+//deleting a product
+
+exports.deleteProduct = async (req, res, next) => {
+    
+    let product = await Product.findById(req.params.id)
+    if(!product) {
+        return next(new Error("Product not found", 404));
+    }
+
+    await product.remove();
+    res.status(200).json({
+        success:"true",
+        message:"Product Deleted"
     })
 }
