08dc4317599ba9850d8b43ebbedaa16001866d12
jay459 <anigounigoud@gmail.com>
2021-01-31 22:34:40 +0530

    middleware files

---
+++ backend/middleware/errormiddleware.js
@@ -0,0 +1,11 @@
+const ErrorHandler = require('../utils/errorhandler');
+
+module.exports = (err, req, res, next) => {
+    err.statuscode = err.statuscode || 500;
+    err.message = err.message || "Internal Server Error";
+
+    res.status(err.statuscode).json({
+        success: false,
+        error: err.message
+    })
+}
