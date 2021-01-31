aac7cd2ac172ffa7c57bdbf3eb009a5861cfe4fb
jay459 <anigounigoud@gmail.com>
2021-01-31 22:39:08 +0530

    utils

---
+++ backend/utils/seeder.js
@@ -0,0 +1,22 @@
+const Product = require('../models/productModel');
+require('dotenv').config({path: 'backend/.env'});
+const db = require('../database/db');
+
+const products = require('../data/product.json');
+
+
+const seedProducts = async (req,res,next) => {
+    try {
+        await Product.deleteMany();
+        console.log("deleted products")
+
+        await Product.insertMany(products);
+        console.log("successfully added all the products")
+
+    } catch (error) {
+        console.log(error);
+        process.exit();
+    }
+}
+
+seedProducts()
