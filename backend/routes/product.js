d17c6aecf417e1c3531a2e07625fecda1c45a768
jay459 <anigounigoud@gmail.com>
2021-01-31 22:37:59 +0530

    modified routes

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -7,4 +7,10 @@ router.get('/getallproducts', productcontroller.getAllProuducts);
 
 router.post('/newproduct', productcontroller.newProduct);
 
+router.get('/singleproduct/:id', productcontroller.getSingleProduct);
+
+router.put('/updateproduct/:id', productcontroller.updateProductById);
+
+router.delete('/deleteproduct/:id', productcontroller.deleteProduct);
+
 module.exports = router;
