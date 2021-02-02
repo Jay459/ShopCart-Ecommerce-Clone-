b439271ed42b678b2aaa7c885db0a7267796b910
jay459 <anigounigoud@gmail.com>
2021-02-02 20:36:17 +0530

    changes made

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -13,6 +13,20 @@ class APIFeatures {
         this.query = this.query.find({...keyword});
         return this;
     }
+
+    filter(){
+        const querycopy = {...this.querystr};
+        const removefields = ['keyword','limit','page'];
+        //removing fields from query string
+        removefields.forEach(el => delete querycopy[el])
+
+        // adding extra filter for price and rating
+        let querystr = JSON.stringify(querycopy);
+        querystr = querystr.replace(/\b(gt|gte|lte|lt)\b/g, match=> `$${match}`);
+
+        this.query = this.query.find(JSON.parse(querystr)); 
+        return this
+    }
 }
 
 module.exports = APIFeatures;
