d363a66de40610a103011b8388aed9af0fb5659d
jay459 <anigounigoud@gmail.com>
2021-02-02 16:03:49 +0530

    changes made

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,13 @@ const app = express();
 const errormiddleware = require('../backend/middleware/errormiddleware');
 const productroute = require('./routes/product');
 
+//handling the uncaught exception
 
+process.on('uncaughtException', (err) => {
+    console.log(`ERROR: ${err.message}`);
+    console.log("Shutting down the Server");
+    process.exit(0);
+})
 //config
 require('dotenv').config({path:__dirname + '/.env'}) 
 
@@ -20,6 +26,16 @@ app.use('/product', productroute);
 //middleware
 app.use(errormiddleware);
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server started on port : ${port}`);
 }); 
+
+// Handling Unhandled promise rejection
+
+process.on('unhandledRejection' , (err) => {
+    console.log(`ERROR: ${err.message}`);
+    console.log("Shutting Down the Server Due to Unhandled Rejection");
+    server.close(()=>{
+        process.exit(0);
+    })
+})
