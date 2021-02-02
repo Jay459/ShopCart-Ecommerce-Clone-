39cff5a37d25443fb8b9ee3debcfd9376b9a2e61
jay459 <anigounigoud@gmail.com>
2021-02-02 16:03:57 +0530

    changes made

--- backend/middleware/errormiddleware.js
+++ backend/middleware/errormiddleware.js
@@ -4,6 +4,18 @@ module.exports = (err, req, res, next) => {
     err.statuscode = err.statuscode || 500;
     err.message = err.message || "Internal Server Error";
 
+    // Mongoose invalid Object ID
+    if(err.name === "CastError"){
+        const message = `Resource Not Found Invalid ${err.path}`;
+        error = new ErrorHandler(message , 400);
+    }
+
+    // Mongoose Validation Error
+    if(err.name === "ValidationError"){
+        const message = Object.values(err.errors).map(value =>{value.message})
+        error = new ErrorHandler(message , 400);
+    }
+
     res.status(err.statuscode).json({
         success: false,
         error: err.message
