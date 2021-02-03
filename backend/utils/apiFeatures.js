f22c71882a6a2655c6be75328226146c8dcad566
jay459 <anigounigoud@gmail.com>
2021-02-03 13:01:00 +0530

    changes made

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -27,6 +27,13 @@ class APIFeatures {
         this.query = this.query.find(JSON.parse(querystr)); 
         return this
     }
+    pagination(resPerPage){
+        const currentPage = Number(this.querystr.page) || 1;
+        const skip = resPerPage * (currentPage - 1)
+
+        this.query = this.query.limit(resPerPage).skip(skip)
+        return this;
+    }
 }
 
 module.exports = APIFeatures;
