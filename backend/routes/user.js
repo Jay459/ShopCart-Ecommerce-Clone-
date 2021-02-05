4f66103bc16c2a43abfedd1fa007de8e2a79e567
jay459 <anigounigoud@gmail.com>
2021-02-05 20:07:01 +0530

    user routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,4 +6,10 @@ router.post('/register',usercontroller.userRegister);
 
 router.post('/login',usercontroller.userLogin);
 
+router.get('/logout',usercontroller.logout);
+
+router.put('/password/forgot', usercontroller.forgotPassword);
+
+router.put('/password/reset/:token',usercontroller.resetPassword);
+
 module.exports = router
