9d01350bc29f5cf4505dba7e430557e39bf98dac
jay459 <anigounigoud@gmail.com>
2021-02-05 20:04:39 +0530

    modified

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,6 +5,7 @@ const APIFeatures  =require('../utils/apiFeatures');
 //creating new product
 
 exports.newProduct = catchAsynErrors (async (req,res,next) => {
+    req.body.user = req.user._id;
     const product = await Product.create(req.body);
     res.status(200).json({ 
         success: true,
