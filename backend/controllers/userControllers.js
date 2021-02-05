e7b4a480a022c63349d54efc21e01c416f02983b
jay459 <anigounigoud@gmail.com>
2021-02-05 20:04:47 +0530

    modified

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,10 +1,11 @@
 const User = require('../models/userModel');
 const ErrorHandler = require('../utils/errorhandler');
-const catchAsynErrors  = require('../middleware/catchAsyncErrors');
+const catchAsyncErrors  = require('../middleware/catchAsyncErrors');
 const sendToken = require('../utils/jwtToken');
+const sendEmail = require('../utils/sendEmail');
+const crypto = require('crypto');
 
-
-exports.userRegister = catchAsynErrors (async (req, res, next) => {
+exports.userRegister = catchAsyncErrors (async (req, res, next) => {
    const  {name , email, password} = (req.body);
 
    const user = await User.create({
@@ -20,7 +21,7 @@ exports.userRegister = catchAsynErrors (async (req, res, next) => {
    sendToken(user,200 ,res);
 })
 
-exports.userLogin = catchAsynErrors (async (req, res, next) => {
+exports.userLogin = catchAsyncErrors (async (req, res, next) => {
     const {email , password} = (req.body);
 
     if(!email || !password){
@@ -40,4 +41,74 @@ exports.userLogin = catchAsynErrors (async (req, res, next) => {
     }
 
     sendToken(user,200 ,res);
+})
+
+exports.logout = catchAsyncErrors(async (req, res, next)=>{
+    res.cookie('token',null,{
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+    res.status(200).json({
+        success: true,
+        message:'logged Out'
+    })
+})
+
+exports.forgotPassword = catchAsyncErrors (async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email })
+
+    if(!user) {
+        return next(new ErrorHandler("Invalid Email",404));
+    }
+
+    const resetToken = user.resetTokenPassword();
+    await user.save({validateBeforeSave:false});
+
+    const url = `${req.protocol}://${req.get('host')}/user/password/reset/${resetToken}`
+
+    const message = `\n\nYour token for password reset is following:\n\n${url} if you havent requested for reset password then please ignore this`
+
+    try {
+
+        await sendEmail({
+            email: user.email,
+            subject : "Shopcart Password Reset", 
+            message
+        })
+
+        res.status(200).json({
+            success: true,
+            message: `Password sent to ${user.email}`
+        })
+        
+    } catch (error) {
+        resetPasswordToken = undefined
+        resetPasswordExpiry = undefined
+        await user.save({validateBeforeSave:false})
+        return next(new ErrorHandler(error.message,500))
+    }
+})
+
+exports.resetPassword = catchAsyncErrors (async (req, res, next) => {
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpiry : { $gt :Date.now()}
+    })
+    if(!user) {
+        return next(new ErrorHandler('Password reset token has been expired',400));
+    }
+
+    if(req.body.password !== req.body.confirmPassword) {
+        return next(new ErrorHandler('Passwords Doesnot Match',400));
+    }
+
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined
+    user.resetPasswordExpiry = undefined
+
+    await user.save();
+
+    sendToken(user,200,res);
+
 })
