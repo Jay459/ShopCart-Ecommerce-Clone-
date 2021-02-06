41d9861ce004338e949d95b8865820d1147f958f
jay459 <anigounigoud@gmail.com>
2021-02-06 21:50:11 +0530

    modifications

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router();
 const usercontroller = require('../controllers/userControllers');
+const auth = require('../middleware/auth');
 
 router.post('/register',usercontroller.userRegister);
 
@@ -11,5 +12,19 @@ router.get('/logout',usercontroller.logout);
 router.put('/password/forgot', usercontroller.forgotPassword);
 
 router.put('/password/reset/:token',usercontroller.resetPassword);
+
+router.get('/me', auth.isAuthenticatedUser , usercontroller.getUserProfile)
+
+router.put('/changepassword', auth.isAuthenticatedUser , usercontroller.changePassword)
+
+router.put('/updateprofile', auth.isAuthenticatedUser , usercontroller.updateUserProfile)
+
+router.get('/admin/getallusers', auth.isAuthenticatedUser , auth.authRoles('admin') , usercontroller.getAllUsers);
+
+router.get('/admin/getuser/:id', auth.isAuthenticatedUser , auth.authRoles('admin') , usercontroller.getUserById);
+
+router.put('/admin/updateuser/:id', auth.isAuthenticatedUser , auth.authRoles('admin') , usercontroller.updateUserProfileById);
+
+router.delete('/admin/deleteuser/:id', auth.isAuthenticatedUser , auth.authRoles('admin') , usercontroller.deleteUserById);
 
 module.exports = router
