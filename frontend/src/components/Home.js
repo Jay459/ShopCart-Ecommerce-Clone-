7f2dfbfa9e28e81cd8cd4f140b1d16fef2f952dc
jay459 <anigounigoud@gmail.com>
2021-02-14 16:29:11 +0530

    modified components

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,7 @@
 import React , { Fragment , useEffect } from 'react'
 import MetaData from '../components/layouts/MetaData'
-
+import Product from './product/Product'
+import Loader from './layouts/Loader'
 import {useDispatch , useSelector} from 'react-redux'
 import {getProducts} from '../actions/productActions';
 const Home = () => {
@@ -15,37 +16,19 @@ const Home = () => {
 
     return (
         <Fragment>
-            <MetaData title = {'Home'}/>
-            <h1 id="products_heading">Latest Products</h1>
-            <section id="products" className="container mt-5">
-            <div className="row">
-                {products && products.map(product => (
-                    <div key = {product._id} className="col-sm-12 col-md-6 col-lg-3 my-3">
-                    <div className="card p-3 rounded">
-                        <img
-                        className="card-img-top mx-auto"
-                        src={product.image[0].url}
-                        />
-                        <div className="card-body d-flex flex-column">
-                        <h5 className="card-title">
-                            <a href="">{product.name}</a>
-                        </h5>
-                        <div className="ratings mt-auto">
-                            <div className="rating-outer">
-                            <div className="rating-inner"></div>
-                            </div>
-                            <span id="no_of_reviews">({product.noOfReviews} Reviews)</span>
+            {loading ? <Loader/> : (
+                <Fragment>
+                    <MetaData title = {'Home'}/>
+                        <h1 id="products_heading">Latest Products</h1>
+                        <section id="products" className="container mt-5">
+                        <div className="row">
+                            {products && products.map(product => (
+                                <Product key = {product._id} product={product}/>
+                            ))}   
                         </div>
-                        <p className="card-text">{product.price} $</p>
-                        <a href="#" id="view_btn" className="btn btn-block">View Details</a>
-                        </div>
-                    </div>
-                    </div>   
-                ))}
-                
-            </div>
-
-    </section>
+                    </section>
+                </Fragment>
+            )}
         </Fragment>
     )
 }
