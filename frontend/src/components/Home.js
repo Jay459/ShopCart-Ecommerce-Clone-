cb3de81c6eabf1a65605d8e12f022a3830f64bf2
jay459 <anigounigoud@gmail.com>
2021-02-16 17:45:00 +0530

    components

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,15 +4,22 @@ import Product from './product/Product'
 import Loader from './layouts/Loader'
 import {useDispatch , useSelector} from 'react-redux'
 import {getProducts} from '../actions/productActions';
+import { useAlert } from 'react-alert' ;
+
 const Home = () => {
 
+    const alert = useAlert();
     const dispatch = useDispatch();
     
     const {loading , products , error, productscount} = useSelector(state => state.products)
 
     useEffect(() => {
+        if(error){
+            return alert.error(error)
+        }
+
         dispatch(getProducts())
-    }, [dispatch])
+    }, [dispatch , alert ,error])
 
     return (
         <Fragment>
