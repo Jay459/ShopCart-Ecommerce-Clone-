f995a8870d4497f6899157863948ae17375d96e8
jay459 <anigounigoud@gmail.com>
2021-02-16 17:45:59 +0530

    reducers

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,11 +1,15 @@
 import {ALL_PRODUCT_REQUEST,
     ALL_PRODUCT_SUCCESS,
     ALL_PRODUCT_FAIL,
-    CLEAR_ERRORS} from '../constants/productConstants'
+    CLEAR_ERRORS,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
+} from '../constants/productConstants'
 
 
 
-const productReducers = (state = {products:[]}, action) =>{
+ export const productReducers = (state = {products:[]}, action) =>{
     switch(action.type){
         case ALL_PRODUCT_REQUEST:
             return {
@@ -34,4 +38,30 @@ const productReducers = (state = {products:[]}, action) =>{
     }
 }
 
-export default productReducers
+export const productDetailsReducer = (state = {product : {}}, action)=>{
+    switch(action.type){
+
+        case PRODUCT_DETAILS_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+        case PRODUCT_DETAILS_SUCCESS:
+            return {
+                loading:false,
+                product: action.payload
+            }
+        case PRODUCT_DETAILS_FAIL:
+            return {
+                ...state,
+                error:action.payload
+            }
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error:null
+            }
+        default:
+            return state
+    }
+}
