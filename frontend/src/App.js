7d186793523984ac026c93975102ea1ff757c5f9
jay459 <anigounigoud@gmail.com>
2021-02-16 17:44:15 +0530

    changes done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router , Route} from 'react-router-dom';
 import Header from '../src/components/layouts/Header'
 import Footer from '../src/components/layouts/Footer'
 import Home from '../src/components/Home'
+import ProductDetails from './components/product/ProductDetails'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header/>
       <div className="container container-fluid" >
         <Route path="/" component = {Home} exact={true} />
+        <Route path="/product/:id" component = {ProductDetails} exact={true} />
       </div>
       <Footer/>
     </div>
