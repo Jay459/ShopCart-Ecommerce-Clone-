e02736b106e9e68a0d902ad78aa9c21f87e60f5b
jay459 <anigounigoud@gmail.com>
2021-02-22 21:27:18 +0530

    Search component

--- frontend/src/components/layouts/Search.js
+++ frontend/src/components/layouts/Search.js
@@ -1,7 +1,7 @@
-import React ,{useState} from 'react'
+import React ,{ useState } from 'react'
 
 const Search = ({history}) => {
-    const [keyword , setKeyword] = useState('');
+    const [keyword , setKeyword] = useState(' ');
 
     const searchHandler = (e) =>{
         e.preventDefault();
