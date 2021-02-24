71fca61007ac781acb55c8ff5bd1138e4b0b1e39
jay459 <anigounigoud@gmail.com>
2021-02-24 22:07:19 +0530

    Home file

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -11,14 +11,19 @@ import { useAlert } from 'react-alert' ;
 import {getProducts} from '../actions/productActions';
 import { useParams } from 'react-router-dom';
 
-// const { createSliderwithTooltip } = Slider;
+const { createSliderWithTooltip } = Slider;
 
-// const Range = createSliderwithTooltip(Slider.Range)
+const Range = createSliderWithTooltip(Slider.Range)
 
 const Home = () => {
     
-    // const [price,setPrice ] = useState([1 , 1000]);
     const [currentPage , setCurrentPage] = useState(1)
+    const [price , setPrice ] = useState([1 , 1000000]);
+    const [category , setCategory] = useState('')
+
+    const categories =[
+        
+    ]
     
     const alert = useAlert();
     const dispatch = useDispatch();
@@ -33,8 +38,8 @@ const Home = () => {
             return alert.error(error)
         }
         
-        dispatch(getProducts(keyword ,currentPage))
-    }, [dispatch , alert ,error ,keyword, currentPage])
+        dispatch(getProducts(keyword ,currentPage , price))
+    }, [dispatch , alert ,error ,keyword, currentPage , price])
     
     function setCurrentPageNo(pageNumber){
         setCurrentPage(pageNumber)
@@ -48,9 +53,44 @@ const Home = () => {
                         <h1 id="products_heading">Latest Products</h1>
                         <section id="products" className="container mt-5">
                         <div className="row">
-                            {products && products.map(product => (
-                                <Product key = {product._id} product={product}/>
-                            ))}   
+
+                            {keyword ? (
+                                <Fragment>
+                                    <div className = "col-6 col-md-3 mt-5 mb-5">
+                                        <div className = "px-5">
+                                            <Range
+                                                marks={{
+                                                    1:'$1',
+                                                    1000000: '$1000000'
+                                                }}
+                                                min = {1}
+                                                max= {1000000}
+                                                defaultValue = {[1 , 1000000]}
+                                                tipFormatter = {value => `$${value}`}
+                                                tipProps = {{
+                                                    placement: 'top',
+                                                    visible:true
+                                                }}
+                                                value = {price}
+                                                onChange = {price=>setPrice(price)}
+                                            />
+                                        </div>
+                                    </div>
+                                    
+                                    <div className = 'col-6 col-md-9'>
+                                        <div className = 'row'>
+                                            {products && products.map(product => (
+                                            <Product key = {product._id} product={product} col={4}/>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </Fragment>
+                            ):(
+                                products && products.map(product => (
+                                    <Product key = {product._id} product={product} col={3} />
+                                ))  
+                            )}
+
                         </div>
                     </section>
                     {resPerPage <= productsCount && (
