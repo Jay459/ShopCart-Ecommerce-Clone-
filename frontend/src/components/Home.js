9786e86ea223bb92c059c16edacc7b814755a330
jay459 <anigounigoud@gmail.com>
2021-02-25 15:16:48 +0530

    Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -20,15 +20,15 @@ const Home = () => {
     const [currentPage , setCurrentPage] = useState(1)
     const [price , setPrice ] = useState([1 , 1000000]);
     const [category , setCategory] = useState('')
-
+    const [rating , setRating] = useState(0)
     const categories =[
-        
+        "Mobiles","Laptops","Electronics","Food","Books","Clothes/Shoes","Beauty/Health","Sports","Outdoor","Home"
     ]
     
     const alert = useAlert();
     const dispatch = useDispatch();
     
-    const {loading , products , error, productsCount, resPerPage} = useSelector(state => state.products)
+    const {loading , products , error, productsCount, resPerPage ,filteredProductsCount} = useSelector(state => state.products)
     
     const params = useParams()
     const keyword = params.keyword
@@ -38,13 +38,19 @@ const Home = () => {
             return alert.error(error)
         }
         
-        dispatch(getProducts(keyword ,currentPage , price))
-    }, [dispatch , alert ,error ,keyword, currentPage , price])
+        dispatch(getProducts(keyword ,currentPage , price ,category,rating))
+    }, [dispatch , alert ,error ,keyword, currentPage , price ,category ,rating])
     
     function setCurrentPageNo(pageNumber){
         setCurrentPage(pageNumber)
     }
 
+    let count = productsCount;
+    if(keyword){
+        count = filteredProductsCount
+    }
+
+    
     return (
         <Fragment>
             {loading ? <Loader/> : (
@@ -74,6 +80,59 @@ const Home = () => {
                                                 value = {price}
                                                 onChange = {price=>setPrice(price)}
                                             />
+
+                                            <hr className = "my-5"/>
+
+                                            <div className = "mt-5">
+                                                <h4 className = "mb-3">
+                                                    Categories
+                                                </h4>
+
+                                                <ul className = "pl-0">
+                                                    {categories.map(category =>(
+                                                        <li 
+                                                            style = {{cursor: 'pointer',
+                                                            listStyleType: 'none'}}
+                                                        key = {category}
+                                                        onClick = {()=>setCategory(category)}
+                                                        >
+                                                            {category}
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            
+                                            </div>
+
+                                            <hr className = "my-3"/>
+
+                                            <div className = "mt-5">
+                                                <h4 className = "mb-3">
+                                                    Ratings
+                                                </h4>
+
+                                                <ul className = "pl-0">
+                                                    {[[5,4,3,2,1]].map(star =>(
+                                                        <li 
+                                                            style = {{cursor: 'pointer',
+                                                            listStyleType: 'none'}}
+                                                        key = {star}
+                                                        onClick = {()=>setRating(star)}
+                                                        >
+                                                            <div className = "rating-outer">
+                                                                <div className = "rating-inner"
+                                                                    style={{
+                                                                        width:`${star*20}%`
+                                                                    }}
+                                                                >
+
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    ))}
+                                                </ul>
+
+                                            </div>
+
                                         </div>
                                     </div>
                                     
@@ -93,7 +152,7 @@ const Home = () => {
 
                         </div>
                     </section>
-                    {resPerPage <= productsCount && (
+                    {resPerPage <= count && (
                         <div className="d-flex justify-content-center mt-5">
                         <Pagination
                             activePage = {currentPage}
