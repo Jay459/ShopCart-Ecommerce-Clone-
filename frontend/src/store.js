95e7c22aa01ffc947f87b80dbc5a6d1748ea21f9
jay459 <anigounigoud@gmail.com>
2021-03-01 18:00:00 +0530

    changes made

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,10 +2,12 @@ import {createStore , combineReducers , applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {productReducers, productDetailsReducer} from './reducers/productReducers'
+import {authReducer} from './reducers/userReducers'
 
 const reducer = combineReducers({
     products: productReducers,
-    productDetails : productDetailsReducer
+    productDetails : productDetailsReducer,
+    auth:authReducer
 })
 
 const initialState ={}
